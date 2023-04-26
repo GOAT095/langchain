78a94e13fe01f6a8ef6af6f11c23dbfb68475d95
GOAT095 <hakimoxxx@gmail.com>
2023-04-26 10:23:33 +0100

    start

--- conversational-memory.py
+++ conversational-memory.py
@@ -1,10 +1,13 @@
 from langchain import OpenAI
 from langchain.chains import ConversationChain
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # initialization of the large language model in this case openai lm
 llm = OpenAI(
 	temperature=0,
-	openai_api_key="###", #the open ai key
+	openai_api_key= os.getenv('OPENAI_API_KEY'), #the open ai key
 	model_name="text-davinci-003"
 )
 # initialization the conversation chain
